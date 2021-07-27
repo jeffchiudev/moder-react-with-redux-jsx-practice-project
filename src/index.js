e54b1bb4bd1ead0b781649e41178976ace5dafad
Jeff Chiu <jwchiu82@gmail.com>
2021-07-26 19:18:55 -0700

    add index/js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import ReactDom from "react-dom";
 
 // create a react component
 const App = () => {
+  const buttonText = "Click me";
+  const labelText = "Enter name:";
+
   return (
     <div>
-      <label class="label" for="name">
-        Enter name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style="background-color: blue; color: white;">Submit</button>
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
     </div>
   );
 };
